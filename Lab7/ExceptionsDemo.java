3733d21f1e3aeb71b497a14442f830ff372a897b
ShashwatParajuli <shashwatparajuli.cs24@bmsce.ac.in>
2025-11-25 11:28:35 +0530

    Add files via upload

---
+++ Lab7/ExceptionsDemo.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+class WrongAgeException extends Exception {
+    public WrongAgeException(String message) {
+        super(message);
+    }
+}
+
+class Father {
+    protected int fatherAge;
+
+    public Father(int age) throws WrongAgeException {
+        if (age < 0) {
+            throw new WrongAgeException("Father's age cannot be negative!");
+        }
+        this.fatherAge = age;
+        System.out.println("Father's age: " + fatherAge);
+    }
+}
+
+class Son extends Father {
+    private int sonAge;
+
+    public Son(int fatherAge, int sonAge) throws WrongAgeException {
+        super(fatherAge); // Calls Father constructor
+
+        if (sonAge < 0) {
+            throw new WrongAgeException("Son's age cannot be negative!");
+        }
+
+        if (sonAge >= fatherAge) {
+            throw new WrongAgeException("Son's age cannot be greater than or equal to Father's age!");
+        }
+
+        this.sonAge = sonAge;
+        System.out.println("Son's age: " + sonAge);
+    }
+}
+
+public class ExceptionsDemo {
+    public static void main(String[] args) {
+        try {
+            // Test cases
+            Son s1 = new Son(35, 40);
+
+        } catch (WrongAgeException e) {
+            System.out.println("Exception caught: " + e.getMessage());
+        }
+    }
+}
